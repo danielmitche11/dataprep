677413bf27dae7a62cd62cdfbfef861463acfffa
danielmitchell <daniel423@revature.net>
2024-03-04 09:20:53 -0600

    Adds clean data function

--- dataprep.py
+++ dataprep.py
@@ -2,7 +2,16 @@ import pandas as pd
 import nltk
 
 def import_data(path):
-        df = pd.read_csv("path")
+        #Converts .csv data to Pandas dataframe
+        df = pd.read_csv(path)
+
+        return df
+
+def clean_data(df):
+        #Converts all text to lowercase
+        df['Text'] = df['Text'].str.lower()
+        #Removes excess whitespace
+        df['Text'] = ' '.join(df['Text'].str.split())
 
         return df
 
